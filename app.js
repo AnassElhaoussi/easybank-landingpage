7471c61645ae0e527066329e7e2de8edee1d6da7
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-03-18 23:20:45 +0100

    i did some changes on the mobile version of the nav barand i added the main section

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const navButton = document.querySelector(".nav-button")
-const links = document.querySelector(".links")
-
+const navButton = document.querySelector(".nav-btn")
+const menuLinks = document.querySelector(".links")
+const links = document.querySelectorAll(".links")
 
 
 navButton.addEventListener("click", () => {
-    links.classList.toggle("fadeIn")
+
+    
+
+    
+    menuLinks.classList.toggle("isactive")
+    menuLinks.style.animation = "navAnimation 0.3s ease"
 })
-
-
-
