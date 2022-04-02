a5f112205a3aaf69fa301f940e95e520505125cb
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-04-02 12:29:27 +0200

    I added responsive design for big screen devices

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const links = document.querySelectorAll(".links")
 navButton.addEventListener("click", () => {
 
     
-
-    
     menuLinks.classList.toggle("isactive")
     menuLinks.style.animation = "navAnimation 0.3s ease"
 })
